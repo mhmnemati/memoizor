46d8cdf614833972aa6c64f4bc88c6c87f7079b8
KoLiBer <koliberr136a1@gmail.com>
2019-06-24 15:42:11 +0430

    build: package.json add clean, change ts declaration generation config

--- build/src/memoize.d.ts
+++
@@ -1,5 +0,0 @@
-export interface MemoizeConfig {
-}
-export declare function memoize(config?: MemoizeConfig): (target: any, propertyKey: string | symbol, descriptor: PropertyDescriptor) => {
-    value: (...args: any[]) => Promise<any>;
-};
--- build/test/memoize.test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var src_1 = require("../src");
-test("memoize(...) test", function () {
-    var fn = function (arg1, arg2, arg3) { };
-    var memoized_fn = src_1.memoize({});
-});
